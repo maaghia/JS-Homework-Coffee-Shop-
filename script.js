03d2761c71592c0079f9c9941b8cb13547fc5947
maaghia <toubalmaria23@gmail.com>
2022-09-08 18:15:13 +0100

    try

--- script.js
+++ script.js
@@ -40,16 +40,57 @@ class coffeeShop{
     buyCoins(amount, account){
         //buying coins in a currency
     }
-    async orderCoffee(coffee){
-        //return coffeeType
-        //add coin bonus based on the account type
+
+    
+      
+}
+const orderCoffee = async () => {
+    try {
+        let res = await fetch('/coffee.json');
+        let info = await res.json();
+        console.log(info)
+        let data = info.coffee; 
+        console.log(data)
+    } catch (error) {
+        console.log(error)
     }
+    //return coffeeType
+    //add coin bonus based on the account type
 }
 const billingMaria = {
     creditCardNumber : 00000001,
     expiryDate : 31/12/2022,
 };
-let myAccount = new Account(11, maaghia, Maria, corso, billingMaria, 100);
-console.log(myAccount.coffeeCoins)
+let myAccount = new Account(11, "maaghia", "Maria", "corso", billingMaria, 100);
+//console.log(myAccount.coffeeCoins)
 myAccount.addCoins(20, myAccount)
-console.log(myAccount.coffeeCoins)
+//console.log(myAccount.coffeeCoins)
+
+const displayMenu = async () => {
+    try{
+        let res = await fetch("coffee.json");
+        let info = await res.json();
+
+        let data = info.coffee;
+        console.log(data);
+        data.forEach((coffee) => {
+            //Create a div
+            let div = document.createElement("div");
+            //Add 'user' class to div
+            div.className = "coffee";
+            //Add innerHTML to div
+            div.innerHTML = `
+            <img src="${coffee.image}" alt="" />
+            <div>
+                <span class="type">${coffee.type} </span>
+                <span class="price-bonus">${coffee.price} USD / ${coffee.bonus} BP</span>
+            </div>
+            `;
+            //Add div as a child for 'users-list' using appnedChild(node)
+            document.getElementById("users-list").appendChild(div);
+        });
+    }
+    catch(error){
+        console.log(error);
+    }
+}
